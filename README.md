73f6bcbdfc7919b8b1c51c49f70f0d5328d0776d
Michael O'Neill <michael.oneill.2020@mumail.ie>
2024-01-28 20:01:02 +0000

    implement better ui/arg-parsing

--- README.md
+++ README.md
@@ -1,38 +1,43 @@
 # paperboy
 
-Download Maynooth University past exam papers with this command line program
+```
+usage: paperboy.py [-h] [-l MINYEAR] [-u MAXYEAR] [-r] [-s] module cookie
 
+Get Maynooth University exam papers.
 
-## Usage
+positional arguments:
+  module                the code of the module you want papers for e.g. 'CS211'
+  cookie                a JSON file with the key and value of your session cookie OR the key and value as separated by '='
 
-You will need to create a `config.json` file with the cookie that grants access to the service. Place this file at the root directory of this repo.
+options:
+  -h, --help            show this help message and exit
+  -l MINYEAR, --minyear MINYEAR
+                        the inclusive lower bound of the range of years you want papers for
+  -u MAXYEAR, --maxyear MAXYEAR
+                        the non-inclusive upper bound of the range of years you want papers for
+  -r, --noresits        exclude papers from Autumn exam periods
+  -s, --save            save the exam papers to current working directory instead of printing links to stdout
+```
 
-1. In Chrome, log in to the [exam papers website](https://www.maynoothuniversity.ie/library/exam-papers)
+The `cookie` argument is a path to a JSON file containing the name and value of your session cookie for the exam papers site. The site uses this cookie to authenticate you. Follow the steps below to get this cookie:
+
+1. In Chrome, log in to the 
+   [exam papers website](https://www.maynoothuniversity.ie/library/exam-papers)
+
 2. Press F12 to open Developer Tools and go to the Application tab
-3. Go to Cookies >> https://www.maynoothuniversity.ie and copy and paste the Name and Value of the cookie into your config file
-4. Don't share this cookie or accidentally push it to any repo
+
+3. Go to `Cookies` > `https://www.maynoothuniversity.ie` and copy and paste the name and value
+   of the session cookie into your cookie file
+
+This cookie will expire after some time and you will need to follow these steps again.
+
+**Don't share this cookie with anyone or accidentally push it to a repo**
 
 ![Chrome Developer Tools screenshot](images/devtools.png)
 
+#### Example cookie file
 ```json
-// config.json
 {
-    "cookies": {
-        "cookie_name": "cookie_value"
-    }
+    "COOKIE_NAME": "COOKIE_VALUE"
 }
-```
-
-## Argumemts
-
-1. Module code e.g. CS404
-2. Year range lower bound (inclusive)
-3. Year range upper bound (non-inclusive)
-
-E.g.
-
-```
-python ./paperboy.py CS434 2010 2015
-python ./paperboy.py CS434 2018
-python ./paperboy.py MP472
 ```
--- paperboy.py
+++ paperboy.py
@@ -1,42 +1,13 @@
-import sys
-from enum import Enum
-from bs4 import BeautifulSoup as bs
 import requests
 import json
-from datetime import datetime
+import os.path
+from bs4 import BeautifulSoup as bs
+from argparse import ArgumentParser, ArgumentError
 
 
-ExamPeriod = Enum('ExamPeriod', ['January', 'Summer', 'Autumn'])
-
-now = datetime.now()
-
-cookies={}
-with open('config.json', 'r') as config_file:
-    config_data = json.load(config_file)
-    cookies = config_data['cookies']
-
-
-def get_papers(module:str, min_year:int=2000, max_year:int=now.year, 
-               periods:list=['January','Summer','Autumn']):
-    if max_year <= min_year:
-        raise Exception('Invalid year range')
-    soup = _get_page_soup(module)
-    papers = _extract_links(soup)
-    for year in range(min_year, max_year):
-        year = f"{year}"
-        if year not in papers.keys():
-            continue
-        for period in periods:
-            if period not in papers[year].keys():
-                continue
-            paper = papers[year][period]
-            _download_pdf(paper['url'], paper['filename'])
-            print("Saved file:", paper['filename'])
-
-
-def _get_page_soup(module: str) -> bs:
+def _get_page_soup(module: str, cookie: dict) -> bs:
     url = f"https://www.maynoothuniversity.ie/library/exam-papers?code_value_1={module}"
-    response = requests.get(url, cookies=cookies)
+    response = requests.get(url, cookies=cookie)
     if response.ok:
         data = response.content.decode(encoding='ansi')
         soup = bs(data, "html.parser")
@@ -45,13 +16,14 @@ def _get_page_soup(module: str) -> bs:
         raise Exception('HTTP request failed: {response}', response)
 
 
-def _extract_links(soup: bs) -> dict:
+def _extract_papers_details(soup: bs) -> dict:
     d = {}
     links = soup.find_all("span", {'class': "file"})
     links = [link.a for link in links]
     for link in links:
         filename = link.text.strip()
         year, module, period = filename.rstrip('.pdf').split('-')
+        year = int(year)
         url = link['href']
         if year not in d.keys():
             d[year] = {}
@@ -59,8 +31,8 @@ def _extract_links(soup: bs) -> dict:
     return d
     
 
-def _download_pdf(url, filename):
-    response = requests.get(url, cookies=cookies)
+def _download_pdf(url, filename, cookie):
+    response = requests.get(url, cookies=cookie)
     with open(filename, mode='wb') as file:
         file.write(response.content)
 
@@ -70,18 +42,77 @@ def _prettyprint_dict(d: dict):
 
 
 def main():
-    module=""
-    min_year=2000
-    max_year=now.year
-    if len(sys.argv) > 1:
-        module = sys.argv[1]
+    parser = ArgumentParser(
+        description="Get Maynooth University exam papers.")
+    
+    parser.add_argument('module', type=str,
+                        help="the code of the module you want papers for "
+                        "e.g. 'CS211'")
+    parser.add_argument('cookie', type=str,
+                        help="a JSON file with the key and value of your "
+                        "session cookie OR the key and value as separated by '='")
+    parser.add_argument('-l', '--minyear', type=int,
+                        help="the inclusive lower bound of the range of years you "
+                        "want papers for")
+    parser.add_argument('-u', '--maxyear', type=int,
+                        help="the non-inclusive upper bound of the "
+                        "range of years you want papers for")
+    parser.add_argument('-r', '--noresits', action='store_true',
+                        help="exclude papers from Autumn exam periods")
+    parser.add_argument('-s', '--save', action='store_true',
+                        help="save the exam papers to current working directory "
+                        "instead of printing links to stdout")
+    
+    args = parser.parse_args()
+    
+    if os.path.isfile(args.cookie):
+        with open(args.cookie, 'r') as cookiefile:
+            cookie = json.load(cookiefile)
     else:
-        print("You must provide atleast the module code")
-    if len(sys.argv) > 2:
-        min_year = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        max_year = int(sys.argv[3])
-    get_papers(module, min_year, max_year)
+        parts = args.cookie.split('=')
+        if len(parts) == 2:
+            name, value = parts
+            cookie = { name: value }
+        else:
+            raise ArgumentError('inavlid session cookie', 
+            argument=args.cookie, parser=parser)
+        
+    try:
+        soup = _get_page_soup(args.module, cookie)
+    except Exception:
+        print('error: failed to scrape webpage')
+        exit(1)
+    
+    papers = _extract_papers_details(soup)
+
+    if args.minyear:
+        def year_filter(pair):
+            year = pair[0]
+            if args.minyear and year < args.minyear:
+                return False
+            if args.maxyear and year >= args.maxyear:
+                return False
+            return True
+        papers = dict(filter(year_filter, papers.items()))
+    
+    if args.noresits:
+        def period_filter(pair):
+            period = pair[0]
+            return period != 'Autumn' and period != 'Repeat'
+        for pair in papers.items():
+            year, value = pair
+            papers[year] = dict(filter(period_filter, papers[year].items()))
+    
+    for year in papers.keys():
+        for period in papers[year].keys():
+            url = papers[year][period]['url']
+            filename = papers[year][period]['filename']
+            if args.save:
+                _download_pdf(url, filename, cookie=cookie)
+                print(f"saved {filename}")
+            else:
+                print(url)
+
 
 if __name__ == "__main__":
     main()
